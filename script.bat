5e149bcb234e9e0390ab58157c2869f1b826eb26
DerekAllenPerkins <69873110+DerekAllenPerkins@users.noreply.github.com>
2023-10-18 16:41:27 -0400

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,13 @@
+@echo off
+echo [+] Changing DNS servers to OpenDNS Family Shield...
+netsh interface ipv4 set dns "Ethernet" static 208.67.222.123 primary
+netsh interface ipv4 add dns "Ethernet" 208.67.220.123 index=2
+netsh interface ipv4 set dns "Ethernet 2" static 208.67.222.123 primary
+netsh interface ipv4 add dns "Ethernet 2" 208.67.220.123 index=2
+netsh interface ipv4 set dns "Wi-Fi" static 208.67.222.123 primary
+netsh interface ipv4 add dns "Wi-Fi" 208.67.220.123 index=2
+netsh interface ipv4 set dns "Local Area Connection" static 208.67.222.123 primary
+netsh interface ipv4 add dns "Local Area Connection" 208.67.220.123 index=2
+netsh interface ipv4 set dns "Local Area Connection 2" static 208.67.222.123 primary
+netsh interface ipv4 add dns "Local Area Connection 2" 208.67.220.123 index=2
+echo [+] DNS servers changed to OpenDNS Family Shield.
